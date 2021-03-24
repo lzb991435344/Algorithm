adc9afaaa15fe7c4d31b0862a6d90e7f6a383ec7
zhibiaoLiao <991435344@qq.com>
2021-03-25 00:29:06 +0800

    add:增加题目内容描述

--- Acm/HuaWeiCode/problem_02_reverseWords.cpp
+++ Acm/HuaWeiCode/problem_02_reverseWords.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+/*
+2.输出反转的字符串
+题目描述
+写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。（字符串长度不超过1000）
+
+输入描述:
+输入N个字符
+
+输出描述:
+输出该字符串反转后的字符串
+
+示例1
+输入
+abcd
+输出
+dcba
+*/
 int main(){
     std::string  str;
     std::cin>>str; //cin>>str遇到空格就结束
