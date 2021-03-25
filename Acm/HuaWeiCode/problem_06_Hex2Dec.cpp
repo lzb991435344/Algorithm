364cb4927d4a3915cbe41cd7f43f03e61f6bc0b1
zhibiaoLiao <991435344@qq.com>
2021-03-25 17:59:06 +0800

    add:链表排序

--- Acm/HuaWeiCode/problem_06_Hex2Dec.cpp
+++ Acm/HuaWeiCode/problem_06_Hex2Dec.cpp
@@ -1,4 +1,4 @@
-
+/*
 6.题目描述
 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
 
@@ -13,6 +13,7 @@
 0xA
 输出
 10
+*/
 
 #include <iostream>
 #include <string>
@@ -21,26 +22,32 @@ using namespace std;
 
 int main()
 {
-    string str;  
+    string str;
+
+    //输入整个完整的串
     while(getline(cin,str))
-    { 
+    {
         int num = 0;
         int index = 16;
         if(str.length()== 0)
         {
             break;
         }
+        //剔除 ox字符
         for(int i = 2; i < str.length(); i++)
         {
-            if(str[i] >= '0'&& str[i] <= '9')
+            if(str[i] >= '0'&& str[i] <= '9') //数字的情况
             {
                 num = num * 16 + (str[i] - '0');
             }
-            else
+            else //字母转换成十进制，包含大小写
             {
                 num = num * 16 + (str[i] - 'A' + 10);
             }
         }
-       cout<< num <<endl;
+    cout<< num <<endl;
     }
 }
+
+//ps:测试通过
+//进制之间的转换
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # Algorithm
+=======
+# LinuxCLHQ
+ 
+>>>>>>> 5c7c05934aafc7d939bd3f1137d46594ec18c2ca
