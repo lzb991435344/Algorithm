671a486b04d72c45ba112865ed03d7fb9b90261c
zhibiaoLiao <991435344@qq.com>
2021-04-15 00:04:54 +0800

    add:增加搜索树迭代器的实现

---
+++ Acm/LeetCode/Tree/problem_02_BSTIterator.cpp
@@ -0,0 +1,65 @@
+
+
+/*
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+*/
+
+#include <iostream>
+#include <vector>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    vector<int> vec;
+    int i;
+    BSTIterator(TreeNode* root) {
+        i = 0;
+        inorder(root);
+    }
+    void inorder(TreeNode* root){
+        if (!root) return ;
+        inorder(root->left);
+        vec.push_back(root->val);
+        inorder(root->right);
+    }
+    int next() {
+        return vec[i++];
+    }
+    bool hasNext()
+    {
+        return i < vec.size();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
